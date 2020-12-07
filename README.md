2a7a08834eb7a7f4b832c0b66c851a313df162a1
Karthikcoder16 <71510293+Karthikcoder16@users.noreply.github.com>
2020-12-07 18:58:44 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# C34a
-C34
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,57 @@
+var ball;
+var database,position;
+
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(10,10,10,10);
+    ball.shapeColor = "red";
+    database = firebase.database();
+
+    // location  ===>  .ref()
+    //write  ===> .set()
+    //read ===> .on()
+
+    pos = database.ref("ball/position");
+    pos.on("value", readPosition, showError)
+
+}
+// read from database
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+function showError(){
+    console.log("Sorry, Some error");
+
+}
+
+function writePosition(x,y){
+database.ref("ball/position").set({   
+    x : position.x + x,
+    y : position.y + y
+})
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
